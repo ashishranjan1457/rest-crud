a3c4e175dc6624155e32568cb4cf82e878bf1c53
Ubuntu <ubuntu@ip-172-31-18-148.ap-south-1.compute.internal>
2019-01-12 10:36:10 +0000

    Changed server.js file to correctly return output for api/user

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ var connection  = require('express-myconnection'),
 app.use(
 
     connection(mysql,{
-        host     : 'localhost',
-        user     : 'root',
-        password : '',
-        database : 'test',
+        host     : 'employeedb.cuxfzywnjfoh.ap-south-1.rds.amazonaws.com',
+        user     : 'employeedbapp',
+        password : 'employeedbapp',
+        database : 'employees',
         debug    : false //set true if you wanna see debug logger
     },'request')
 
@@ -62,169 +62,19 @@ curut.get(function(req,res,next){
 
         if (err) return next("Cannot Connect");
 
-        var query = conn.query('SELECT * FROM t_user',function(err,rows){
+        var query = conn.query('SELECT * FROM employee_data',function(err,rows){
 
             if(err){
                 console.log(err);
                 return next("Mysql error, check your query");
             }
 
-            res.render('user',{title:"RESTful Crud Example",data:rows});
+	   res.send(rows);
 
          });
 
     });
 
-});
-//post data to DB | POST
-curut.post(function(req,res,next){
-
-    //validation
-    req.assert('name','Name is required').notEmpty();
-    req.assert('email','A valid email is required').isEmail();
-    req.assert('password','Enter a password 6 - 20').len(6,20);
-
-    var errors = req.validationErrors();
-    if(errors){
-        res.status(422).json(errors);
-        return;
-    }
-
-    //get data
-    var data = {
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password
-     };
-
-    //inserting into mysql
-    req.getConnection(function (err, conn){
-
-        if (err) return next("Cannot Connect");
-
-        var query = conn.query("INSERT INTO t_user set ? ",data, function(err, rows){
-
-           if(err){
-                console.log(err);
-                return next("Mysql error, check your query");
-           }
-
-          res.sendStatus(200);
-
-        });
-
-     });
-
-});
-
-
-//now for Single route (GET,DELETE,PUT)
-var curut2 = router.route('/user/:user_id');
-
-/*------------------------------------------------------
-route.all is extremely useful. you can use it to do
-stuffs for specific routes. for example you need to do
-a validation everytime route /api/user/:user_id it hit.
-
-remove curut2.all() if you dont want it
-------------------------------------------------------*/
-curut2.all(function(req,res,next){
-    console.log("You need to smth about curut2 Route ? Do it here");
-    console.log(req.params);
-    next();
-});
-
-//get data to update
-curut2.get(function(req,res,next){
-
-    var user_id = req.params.user_id;
-
-    req.getConnection(function(err,conn){
-
-        if (err) return next("Cannot Connect");
-
-        var query = conn.query("SELECT * FROM t_user WHERE user_id = ? ",[user_id],function(err,rows){
-
-            if(err){
-                console.log(err);
-                return next("Mysql error, check your query");
-            }
-
-            //if user not found
-            if(rows.length < 1)
-                return res.send("User Not found");
-
-            res.render('edit',{title:"Edit user",data:rows});
-        });
-
-    });
-
-});
-
-//update data
-curut2.put(function(req,res,next){
-    var user_id = req.params.user_id;
-
-    //validation
-    req.assert('name','Name is required').notEmpty();
-    req.assert('email','A valid email is required').isEmail();
-    req.assert('password','Enter a password 6 - 20').len(6,20);
-
-    var errors = req.validationErrors();
-    if(errors){
-        res.status(422).json(errors);
-        return;
-    }
-
-    //get data
-    var data = {
-        name:req.body.name,
-        email:req.body.email,
-        password:req.body.password
-     };
-
-    //inserting into mysql
-    req.getConnection(function (err, conn){
-
-        if (err) return next("Cannot Connect");
-
-        var query = conn.query("UPDATE t_user set ? WHERE user_id = ? ",[data,user_id], function(err, rows){
-
-           if(err){
-                console.log(err);
-                return next("Mysql error, check your query");
-           }
-
-          res.sendStatus(200);
-
-        });
-
-     });
-
-});
-
-//delete data
-curut2.delete(function(req,res,next){
-
-    var user_id = req.params.user_id;
-
-     req.getConnection(function (err, conn) {
-
-        if (err) return next("Cannot Connect");
-
-        var query = conn.query("DELETE FROM t_user  WHERE user_id = ? ",[user_id], function(err, rows){
-
-             if(err){
-                console.log(err);
-                return next("Mysql error, check your query");
-             }
-
-             res.sendStatus(200);
-
-        });
-        //console.log(query.sql);
-
-     });
 });
 
 //now we need to apply our router here
@@ -236,3 +86,4 @@ var server = app.listen(3000,function(){
    console.log("Listening to port %s",server.address().port);
 
 });
+
